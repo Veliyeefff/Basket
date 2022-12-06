a39a981d1369bed38eb2c0ea37eb36a83c7210c8
Veliyeefff <6mazgz0@code.edu.az>
2022-12-06 23:42:42 +0400

    Created

--- main.js
+++ main.js
@@ -16,11 +16,9 @@ const objarr = [
     }
 ]
 
-let counter=0;
 let container = document.querySelector(".container")
 let body = document.querySelector("body")
 let basket = document.querySelector("i")
-let span = document.querySelector("span")
 
 window.localStorage.setItem("Products", JSON.stringify(objarr))
 
@@ -40,28 +38,43 @@ objarr.forEach(element => {
     container.appendChild(div)
 })
 
+
 let btn = document.querySelectorAll('[prodID]')
 
-
+let counter = 0;
 let basket_array = []
+let span = document.querySelector("span")
+basket_array = JSON.parse(localStorage.getItem("Basket")) ?? [];
+if(basket_array){
+    basket_array.forEach(element =>{
+        counter += element.count
+    })
+    span.innerText = counter
+}
+
+
 btn.forEach(element =>{
     element.addEventListener("click",function(e){
-            if(basket_array.some(x=> x.product.id == e.target.getAttribute("prodId"))){
-                basket_array.forEach(elem => {
-                    if(elem.product.id == e.target.getAttribute("prodId")){
-                        elem.count+=1
-                    }
-                })
-            }else{
-                let newItem = {
-                    product: objarr.find(x=> x.id == e.target.getAttribute("prodId")),
-                    count: 1
+        if(basket_array.some(x=> x.product.id == e.target.getAttribute("prodId"))){
+            basket_array.forEach(elem => {
+                if(elem.product.id == e.target.getAttribute("prodId")){
+                    elem.count+=1
                 }
-                basket_array.push(newItem)
+            })
+        }else{
+            let newItem = {
+                product: objarr.find(x=> x.id == e.target.getAttribute("prodId")),
+                count: 1
             }
-            localStorage.setItem("basket", JSON.stringify(basket_array))
-        })
+            basket_array.push(newItem)
+        }
+        localStorage.setItem("Basket", JSON.stringify(basket_array))
+     })
 })
+
+
+
+
 
 
 
